435e7ae54d33dd2704ba5dfc4aaeeffe518affac
Manisha Balakumar <manishapbalakumar@gmail.com>
2019-05-24 11:57:50 -0500

    broke testem

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,10 +1,21 @@
 'use strict';
 
 //Complete this algo
-const isLoop = (linkedlist) => {
-
+const isLoop = linkedlist => {
+  const counterObj = {};
+  let currentNode = linkedlist.head;
+  while (currentNode !== null) {
+    if (currentNode.value) {
+      if (counterObj[currentNode.value]) {
+        return true;
+      } else {
+        counterObj[currentNode.value] = 1;
+        currentNode = currentNode.next;
+      }
+      return false;
+    }
+  }
 };
-
 
 /*
 EXTRA CREDIT:
@@ -14,4 +25,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
